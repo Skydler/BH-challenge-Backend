d0e7eb95c74488c50a8a460508f2039894920db6
Leonel <leonelmandarino@gmail.com>
2021-08-03 12:02:32 -0300

    Add product model and resources endpoints

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Resource, Api
+from models.product import Product
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,6 +8,22 @@ api = Api(app)
 products = []
 scheduled_emails = []
 
+
+class ProductResource(Resource):
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+class Email(Resource):
+    def get(self):
+        return scheduled_emails
+
+
+api.add_resource(ProductResource, "/product")
+api.add_resource(Email, "/email_schedule")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ models/product.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, customer_id, product_name, domain, start_date, duration_months):
+        self.customer_id = customer_id
+        self.product_name = product_name
+        self.domain = domain
+        self.start_date = start_date
+        self.duration_months = duration_months
