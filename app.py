ec32ab8ccf26653c900b5ca41bd1384d96560cd4
Leonel <leonelmandarino@gmail.com>
2021-08-03 13:40:00 -0300

    Add email endpoint logic

--- app.py
+++ app.py
@@ -40,12 +40,30 @@ class ProductResource(Resource):
             return "Product not found", 404
 
 
+email_fields = {
+    "customer_id": fields.String,
+    "product_name": fields.String,
+    "domain": fields.String,
+    "email_date": fields.DateTime(dt_format="iso8601"),
+}
+
+
 class Email(Resource):
+    @marshal_with(email_fields)
     def get(self):
         emails = []
         for prod in products:
-            emails += prod.scheduled_emails
-        return emails
+            for email_date in prod.scheduled_emails:
+                emails.append(
+                    {
+                        "customer_id": prod.customer_id,
+                        "product_name": prod.product_name,
+                        "domain": prod.domain,
+                        "email_date": email_date,
+                    }
+                )
+        sorted_emails = sorted(emails, key=lambda x: x["email_date"])
+        return sorted_emails
 
 
 api.add_resource(ProductResource, "/product")
