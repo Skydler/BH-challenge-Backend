1909a5dd2a3ab605f30ef237de36458bbc5dcdca
Leonel <leonelmandarino@gmail.com>
2021-08-03 12:19:02 -0300

    Add creation and deletion of products

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from models.product import Product
 
@@ -11,9 +11,31 @@ scheduled_emails = []
 
 class ProductResource(Resource):
     def post(self):
-        pass
+        data = request.form
+        prod = Product(
+            customer_id=data["customer_id"],
+            product_name=data["product_name"],
+            domain=data["domain"],
+            start_date=data["start_date"],
+            duration_months=data["duration_months"],
+        )
+        products.append(prod)
+        self._schedule_emails(prod)
+        return "", 201
 
     def delete(self):
+        data = request.form
+        product = [
+            prod
+            for prod in products
+            if prod.customer_id == data["customer_id"]
+            and prod.product_name == data["product_name"]
+            and prod.domain == data["domain"]
+        ]
+        products.remove(product[0])
+        return "", 204
+
+    def _schedule_emails(product):
         pass
 
 
